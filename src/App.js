0a846952d6ab726c50d7f9b5fdc0c105b32c5559
CHUNDOOIN <monkey0818@naver.com>
2022-01-27 19:21:52 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom"
+import './App.css'
+
+import Home from "./Home";
+import Week from "./Week";
+import Detail from "./Detail";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="wrap">
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/home" element={<Home />}/>
+                    <Route path="/:day" element={<Detail />}/>
+                </Routes>
+            </div>
+        </div>
+    );
 }
+
+
 
 export default App;
